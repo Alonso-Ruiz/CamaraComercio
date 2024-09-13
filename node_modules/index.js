6a2a0af136732451645044a9eb4173a87e92914d
Alonso-Ruiz <ruizcardenas03@gmail.com>
2024-09-13 12:44:16 -0500

    Trabajo

---
+++ node_modules/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/send_data', express.json(), (req, res) => {
+  const data = req.body.data;
+  io.emit('data from php', data);
+  res.send('Data received');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
